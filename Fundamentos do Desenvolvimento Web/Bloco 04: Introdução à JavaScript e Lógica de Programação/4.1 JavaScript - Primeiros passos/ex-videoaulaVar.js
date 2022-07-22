29cbe41aa6850cfd7a7b0215ddbe5ba37f932b3b
Anabia <anabiacsoares@gmail.com>
2022-07-22 12:48:27 -0300

    exercises 4.1

---
+++ Fundamentos do Desenvolvimento Web/Bloco 04: Introdução à JavaScript e Lógica de Programação/4.1 JavaScript - Primeiros passos/ex-videoaulaVar.js
@@ -0,0 +1,12 @@
+const myName = "Ana";
+
+
+const birthCity = "sjrp";
+
+let birthYear = 2002;
+
+console.log(myName)
+console.log(birthCity)
+console.log(birthYear)
+birthYear = 2030;
+birthCity = "sp";
