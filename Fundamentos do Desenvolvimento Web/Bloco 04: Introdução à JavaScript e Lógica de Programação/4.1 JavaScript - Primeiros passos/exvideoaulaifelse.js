3a07b913ef22af8546e46106297205efc9eeea18
Anabia <anabiacsoares@gmail.com>
2022-07-22 19:01:48 -0300

    exercises dia 1 bloco 4 ifelse

---
+++ Fundamentos do Desenvolvimento Web/Bloco 04: Introdução à JavaScript e Lógica de Programação/4.1 JavaScript - Primeiros passos/exvideoaulaifelse.js
@@ -0,0 +1,12 @@
+let notaCandidata = 69;
+
+if (notaCandidata >= 80) {
+    console.log ("Parabéns, você foi aprovada");
+}
+
+else if (notaCandidata < 80 && notaCandidata >= 60) {
+    console.log("Você está na nossa lista de espera");
+}
+else {
+    console.log("Você foi reprovada");
+}
